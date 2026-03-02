4f445dc08e86c165d4a2848a20e1081bf2136ab9
Yiming Fan <jfan@lcs.on.ca>
2026-03-02 14:42:32 -0500

    Got the tap bar set up

---
+++ MyBigApp/WeatherView.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherView.swift
+//  MyBigApp
+//
+//  Created by Fan Yiming on 2026-03-02.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        NavigationStack{
+            Rectangle()
+                .navigationTitle("Weather +")
+            
+            
+
+        }
+
+    }
+
+}
+
+#Preview {
+    WeatherView()
+}
