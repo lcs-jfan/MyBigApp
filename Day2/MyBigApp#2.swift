5a3c9e3b88a0393932a6fb7bba79ae2ef51b0372
Yiming Fan <jfan@lcs.on.ca>
2026-03-04 08:50:28 -0500

    Block of the list created

---
+++ Day2/MyBigApp#2.swift
@@ -0,0 +1,18 @@
+//
+//  MyBigApp#2.swift
+//  MyBigApp
+//
+//  Created by Fan Yiming on 2026-03-04.
+//
+
+import SwiftUI
+
+struct MyBigApp_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyBigApp_2()
+}
